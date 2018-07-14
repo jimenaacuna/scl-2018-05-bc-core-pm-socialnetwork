75621f66ef4bd8d742aad1d457aa4d0aa982c48e
AriellaRogueN <maitilydevidasi@gmail.com>
2018-07-14 19:21:25 -0400

    enlace a ver perfil desde la lista de usuarios

--- js/friends.js
+++ js/friends.js
@@ -1,3 +1,11 @@
+let currentUser = '';
+window.onload = () => {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = firebase.auth().currentUser.uid
+        }
+    });
+} 
 showUsers = () => {
     console.log('showusers')
     usersContainer.innerHTML = ''
@@ -8,8 +16,16 @@ showUsers = () => {
                 <div class="row">
                 <div class="col-10 profile mx-auto">
                 <img class="profile-photo-list" src="${newUser.val().photoUrl}">
-                </img><span>${newUser.val().displayName}</span>
+                </img>
+                <div class="row">
+                <span>${newUser.val().displayName}</span>
+                </div>
+                <div class="row">
                 <span>${newUser.val().rol}</span>
+                </div>
+                <div class="row">
+                <a href="profile.html?user=${newUser.key}">Ver perfil</a>
+                </div>
                 <button class="btn-primary inline  green-one" onclick="addFriends('${newUser.key}', '${newUser.val().email}')">Agregar a mis amigos</button>
                 </div>
                 </div>
@@ -17,13 +33,11 @@ showUsers = () => {
         });
 }
 addFriends = (keyNewFriend, email) => {
-    let currentUser = firebase.auth().currentUser.uid
     firebase.database().ref(`users/${currentUser}/amigos/${keyNewFriend}`).update({ email: email, uid: keyNewFriend });
 }
 showFriends = () => {
     console.log('showfriends')
     usersContainer.innerHTML = ''
-    let currentUser = firebase.auth().currentUser.uid
     firebase.database().ref(`users/${currentUser}/amigos/`)
         .limitToLast(10)
         .on('child_added', (friend) => {
@@ -34,8 +48,16 @@ showFriends = () => {
                 <div class="row">
                 <div class="col-10 profile mx-auto">
                 <img class="profile-photo-list" src="${user.val().photoUrl}">
-                </img><span>${user.val().displayName}</span>
-                <span>Nutricionista</span>
+                </img>
+                <div class="row">
+                <span>${user.val().displayName}</span>
+                </div>
+                <div class="row">
+                <span>${user.val().rol}</span>
+                </div>
+                <div class="row">
+                <a href="profile.html?user=${user.key}">Ver perfil</a>
+                </div>
                 <button class="btn-primary inline green-one" onclick="deleteFriend('${user.key}')">Eliminar</button>
                 </div>
                 </div>
@@ -45,7 +67,6 @@ showFriends = () => {
 }
 deleteFriend = (userKey)=>{
 console.log('delete' + userKey)
-let currentUser = firebase.auth().currentUser.uid
 firebase.database().ref(`users/${currentUser}/amigos/${userKey}`).remove()
 showFriends()
 }
--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,16 @@
-showProfile = () => {
-    let currentUser = firebase.auth().currentUser.uid
-    firebase.database().ref(`users/${currentUser}`) //ref es la ruta para llegar a los datos
+let currentUser = '';
+window.onload = () => { 
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = firebase.auth().currentUser.uid
+            let params = new URLSearchParams(document.location.search.substring(1));
+            let userKey = params.get("user");
+            showProfile(userKey)
+        } 
+    });
+} 
+showProfile = (userKey) => {
+    firebase.database().ref(`users/${userKey}`) 
         .once('value')
         .then((user) => {
             userName.innerText = user.val().displayName
@@ -13,15 +23,21 @@ showProfile = () => {
         })
         .catch((error) => {
             console.log("Database error > " + JSON.stringify(error));
-        });
+            });
 }
+// cada vez que se cambia el rol se actualiza en la base de datos
 rol.addEventListener("change", function () {
-   console.log('cambio')
-    let currentUser = firebase.auth().currentUser.uid
     firebase.database().ref(`users/${currentUser}`).update({ rol: rol.value });
 });
+// cada vez que se cambia el pais se actualiza en la base de datos
 country.addEventListener("change", function () {
-    console.log('cambio')
-    let currentUser = firebase.auth().currentUser.uid
     firebase.database().ref(`users/${currentUser}`).update({ pais: country.value });
+});
+// para activar el input del rol y que se pueda editar
+enabledRol.addEventListener("click", function () {
+    rol.disabled = false;
+});
+// para activar el input del pais y que se pueda editar
+enabledCountry.addEventListener("click", function () {
+    country.disabled = false;
 });
