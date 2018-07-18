5bde599308dcfb0f801d7a66dae675a20348d737
AriellaRogueN <maitilydevidasi@gmail.com>
2018-07-17 22:26:26 -0400

    Nombre de remitente y destinatario corregido

--- js/messages.js
+++ js/messages.js
@@ -3,14 +3,13 @@ window.onload = () => {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             currentUser = firebase.auth().currentUser.uid
-            showMessages()
+            showMessagesReceived()
             firebase.database().ref(`users/${currentUser.uid}`)
                 .once('value')
                 .then((user) => {
                     fullProfile = user.val()
                     $('.displayName').html(`Bienvenid@: <b> ${fullProfile.displayName} </b>`)
                     $('.imagen').html(`<img class="profile" width="30" src="${fullProfile.photoUrl}">`)
-                    mostrarPublicaciones()
                 })
                 .catch((error) => {
                     console.log("Database error > " + JSON.stringify(error));
@@ -18,8 +17,27 @@ window.onload = () => {
         }
     });
 }
-showMessages = () => {
+showMessagesReceived= () =>{
+        messagesSend.innerHTML = ''
+        messagesReceived.innerHTML = ''
+        firebase.database().ref(`users/${currentUser}/messages-received`)
+            .limitToLast(10)
+            .on('child_added', (user) => {
+                firebase.database().ref(`users/${currentUser}/messages-received/${user.key}`)
+                    .limitToLast(10)
+                    .on('child_added', (message) => {
+                        console.log(message.val())
+                        messagesReceived.innerHTML += `
+                         <div class="msn">
+                    <h5>${message.val().remitente}</h5>
+                    <p>${message.val().mensaje}</p>
+                    </div>`
+                    })
+            })
+}
+showMessagesSend = () => {
     messagesSend.innerHTML = ''
+    messagesReceived.innerHTML = ''
     firebase.database().ref(`users/${currentUser}/messages-send`)
         .limitToLast(10)
         .on('child_added', (user) => {
@@ -28,22 +46,10 @@ showMessages = () => {
                 .on('child_added', (message) => {
                     console.log(message.val())
                     messagesSend.innerHTML += `
-                    <span>${message.val().destino}</span>
-                    <p>${message.val().mensaje}</p>`
+                    <div class="msn">
+                    <h5>${message.val().destino}</h5>
+                    <p>${message.val().mensaje}</p>
+                    </div>`
                 })
-        })
-    messagesReceived.innerHTML = ''
-    firebase.database().ref(`users/${currentUser}/messages-received`)
-        .limitToLast(10)
-        .on('child_added', (user) => {
-            firebase.database().ref(`users/${currentUser}/messages-received/${user.key}`)
-                .limitToLast(10)
-                .on('child_added', (message) => {
-                    console.log(message.val())
-                    messagesReceived.innerHTML += `
-                    <span>${message.val().remitente}</span>
-                    <p>${message.val().mensaje}</p>`
-                })
-        })
-
+        }) 
 }
