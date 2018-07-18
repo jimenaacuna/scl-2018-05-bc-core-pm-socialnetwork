440944a51ae99aecec622c99553034019f5df365
AriellaRogueN <maitilydevidasi@gmail.com>
2018-07-18 17:29:12 -0400

    estructura para test

---
+++ test/auth.spec.js
@@ -0,0 +1,11 @@
+describe('auth', () => {
+  it('debería ser una funcion', () => {
+      assert.isFunction(logoutWithFireBase);
+  });
+
+  it('debería exponer función sortUsers en objeto global', () => {
+      assert.isFunction(facebookLoginwithFireBase);
+  });
+
+
+});
