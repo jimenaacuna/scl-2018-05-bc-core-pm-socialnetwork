aab4e5e90bd64b339c445fe89ab07fc51a7e3d14
AriellaRogueN <maitilydevidasi@gmail.com>
2018-07-19 17:40:51 -0400

    headless

--- test/headless.js
+++ test/headless.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-global.assert = require('chai').assert;
-require('../js/auth');
-=======
 //global.assert = require('chai').assert;
 
 require('../js/auth');
 require('../js/post');
 require('./auth.spec.js');
->>>>>>> df0c5a2abce1d02a34459ff68c0b6344581e779a
